535d71b1490fb54a7c336434756b9d34d8f67588
Dylan <givememissiles@gmail.com>
2026-05-31 15:46:59 -0500

    Added first file

---
+++ AI_Manager.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn
+from Drugs import Object
+
+
+class AIManager:
+    def __init__(player, ais, hidden_size, input_size, output_size, layers):
+        pass
+
+
+class LSTM(nn.Module):
+    pass
+
+
+class AI(Object):
+    pass
