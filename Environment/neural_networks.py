cab3883113c1376a2795f25d7f57ce3c172967af
Dylan <givememissiles@gmail.com>
2026-06-03 09:37:58 -0500

    Added new Files

---
+++ Environment/neural_networks.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+
+
+class LSTM(nn.Module):
+    pass
+
+
+class SimpleNN(nn.Module):
+    pass
---
+++ Environment/objects.py
@@ -0,0 +1,13 @@
+
+
+class Object:
+    pass
+
+class Food(Object):
+    pass
+
+class AI_Agent(Object):
+    pass
+
+class Drug(Object):
+    pass
